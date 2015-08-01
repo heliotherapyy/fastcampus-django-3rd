4d24c16c5d4f1888ed67dcf58493bb72883eaaaa
Chinseok Lee <allieuslee@gmail.com>
2015-08-01 15:55:04 +0900

    photos.views.detail 뷰 구현

--- pystagram/photos/views.py
+++ pystagram/photos/views.py
@@ -7,3 +7,10 @@ def index(request):
     return render(request, 'photos/index.html', {
         'post_list': post_list,
     })
+
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'photos/detail.html', {
+        'post': post,
+    })
